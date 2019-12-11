cdcface008a00bc14e8a20a687842349723e628d
Craig Storer <craigstorer@gmail.com>
2019-12-11 19:24:01 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word (sentence)
  words = []
- words << "sentence"
- words.collect
+ words << sentence
+ words.collect do |x|
+ x.reverse
 end
- return words.reverse
+ return words
+end
 
 # return sentence.reverse
